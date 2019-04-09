fdf300fd92e9c7d3c2fbe736a8df9a054590d0ec
Jason Stroup <jasonstroup12@gmail.com>
2019-04-09 16:26:56 -0600

    built works for now, added routes and styled components

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TM_FILENAME_BASE extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello from Product</h3>
+      </div>
+    )
+  }
+}
