a4bea40cc7ec7d413d81fac43fd79f3743bc898b
Jason Stroup <jasonstroup12@gmail.com>
2019-04-12 02:48:05 -0600

    added styled components to product list and product pages

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,13 +9,37 @@ export default class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-        <div className="img-container p-5" onClick={console.log('you clicked me on the image container')
+        <div className="img-container p-5" onClick={()=> console.log('you clicked me on the image container')
       }>
       <Link to="/details">
         <img src={img} alt="product" className="card-img-top" 
         />
-      </Link>  
+      </Link> 
+      <button className="cart-btn" disabled={inCart ? true : false} onClick={()=>{console.log('added to the cart');
+    }}
+    >
+      {inCart ? (
+         <p className="text-capitalize mb-0" disabled>
+          {" "}
+          in Cart
+         </p>
+        ) : (
+          <i className="fas fa-cart-plus" /> 
+      )} 
+      </button>
         </div>
+
+        {/* card footer */}
+        <div className="card-footer d-flex justify-content-between">
+        <p className="align-self-cetner mb-0">
+        {title}
+        </p>
+        <h5 className="text-blue font-italic mb-0">
+          <span className="mr-1">$</span>
+          {price}
+        </h5>
+        </div>
+
         </div>
       </ProductWrapper>
     );
@@ -23,5 +47,53 @@ export default class Product extends Component {
 }
 
 const ProductWrapper = styled.div`
+.card{
+  border-color:transparent;
+  transition:all .25s linear;
+}
+.card-footer {
+  background: transparent;
+  border-top: transparent;
+  transition: all .25s linear;
+}
+&:hover{
+  .card{
+    border:0.04rem solid rgba(0,0,0,0.2);
+    box-shadow: 2px 2px 5px 0px rgba (0,0,0,0.2);
+  }
+  .card-footer{
+    background:rgba(247,247);
+  }
+}
+.img-container{
+  position:relative;
+  overflow:hidden;
+}
+.card-img-top{
+  transition: all .4s linear;
+}
+.img-container:hover .card-img-top{
+  transform: scale(1.2);
+}
+.cart-btn{
+  position: absolute;
+  bottom:0;
+  right: 0;
+  padding: 0.2rem 0.4 rem;
+  background: var(--lightBlue);
+  border: none;
+  color: var(--mainWhite);
+  font-size: 1.4rem;
+  border-radius: 0.5rem 0;
+  transform: translate(100%, 100%);
+}
+.img-container:hover .cart-btn{
+  transform:translate(0,0);
+  transition: all .4s linear;
+}
+.cart-btn:hover{
+  color:var(--mainBlue);
+  cursor: pointer;
+}
 
-`
+`;
